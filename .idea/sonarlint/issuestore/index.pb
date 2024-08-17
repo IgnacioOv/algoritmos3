c059c9f429a6b64d7503c6fda82052188b4e362e
Ignacio Oliva Velez <ioliva@ibm.com>
2024-08-17 10:52:02 -0300

    feat: init repo

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,5 @@
+
+=
+src/Main.java,9\2\924f089edb4af5ee45c8a9d1129cd28e2b368138
+S
+#src/divideAndConquer/MergeSort.java,f\c\fcc2e7bb9b10bad9d8c255a0035cd13cab1d796e
---
+++ src/divideAndConquer/MergeSort.java
@@ -0,0 +1,16 @@
+package divideAndConquer;
+
+public class MergeSort {
+
+    public static void main(String[] args) {
+        int[] array = { 12, 11, 13, 5, 6, 7 };
+        System.out.println("Given Array");
+        for (int i = 0; i < array.length; i++) {
+            System.out.print(array[i] + " ");
+        }
+        System.out.println("\nOrdered array");
+
+    }
+
+
+}
